fe6c3c6bab4844a3f83af285c81f3c4e6b706346
ericwtlin <eric.wtlin@gmail.com>
2015-08-14 21:27:14 +0800

    优化

--- GetBaiduCoordinates.py
+++ GetBaiduCoordinates.py
@@ -53,58 +53,78 @@ def get_roads_coordinates(geojson_path, output_path):
         'primary_link', 'motorway', 'tertiary_link', 'road'])
     nodes = {}  # 所有节点
     nodes_index = 0  # echarts文件里节点的编号，作为节点名
+    nodes_index_hashmap = {}    #查询节点编号，key=coord_gps,例如“116.3857, 39.90193”，value=node index
     roads = []  # 所选道路
     roads.append([])  # roads[0]为拥堵
     roads.append([])  # roads[1]为缓行
     roads.append([])  # roads[2]为畅通
     count = 0
+    count_repeat = 0
     for line in road_file:
         count += 1
-        print(count)
-        if(count > 1000):
-            break
+        
+        if(count % 100 == 0):
+            print("line_num:" + str(count))
+            print("count_repeat:" + str(count_repeat))
+            
         if (line.startswith("{ \"type\": \"Feature\", \"properties\": { \"osm_id\"")):  # 以这个作为道路行数据的标志
             if(line.endswith(',\n')):
-               line = line.rstrip(', \n')  # 去除末尾可能有的逗号
+                line = line.rstrip(', \n')  # 去除末尾可能有的逗号
             road_data = json.loads(line)
             # properties = road_data['properties']
             # road_type = properties['type']
             if (road_data['properties']['type'] in road_types_needed):  # 只取出大路
-               coordinates_gps = road_data['geometry'] ['coordinates']
-               coord_gps_param = ''
-               # 转换坐标
-               for coord in coordinates_gps:
-                   if len(coord_gps_param) == 0:
-                       coord_gps_param = str(coord[0]) + ',' + str(coord[1])
-                   else:
-                       coord_gps_param = coord_gps_param + ';' + str(coord[0]) + ',' + str(coord[1])
+                coordinates_gps = road_data['geometry'] ['coordinates']
+                coord_gps_param = ''
+                # 转换坐标
+                for coord in coordinates_gps:
+                    if len(coord_gps_param) == 0:
+                        coord_gps_param = str(coord[0]) + ',' + str(coord[1])
+                    else:
+                        coord_gps_param = coord_gps_param + ';' + str(coord[0]) + ',' + str(coord[1])
                         
-               quiry_result = convert_coordinates(coord_gps_param)
-               # print(repr(quiry_result))
-               coordinates_baidu = []
-               # 获取百度坐标列表
-               for node in quiry_result:
-                   coordinates_baidu.append([node['x'], node['y']])
+                quiry_result = convert_coordinates(coord_gps_param)
+                # print(repr(quiry_result))
+                coordinates_baidu = []
+                # 获取百度坐标列表
+                for node in quiry_result:
+                    coordinates_baidu.append([round(node['x'], 5), round(node['y'], 5)])
                
-               # print(repr(coordinates_baidu))
-               road_status = random.randint(0, 2)  # 随机道路状态
-               for index in range(len(coordinates_baidu)):
-                   if (index != len(coordinates_baidu) - 1) :
-                       # fragment是一个线路片段
-                       fragment = []
-                       fragment_head = {}
-                       fragment_head["name"] = str(nodes_index)
-                       fragment_tail = {}
-                       fragment_tail["name"] = str(nodes_index + 1)
-                       fragment.append(fragment_head)
-                       fragment.append(fragment_tail)
-                       roads[road_status].append(fragment)
-                   
-                   nodes[str(nodes_index)] = coordinates_baidu[index]
-                   nodes_index += 1
-               # nodes[str(nodes_index)] = coordinates_baidu[len(coordinates_baidu) - 1]
-    print("nodes_num:" + str(nodes_index))
+                # print(repr(coordinates_baidu))
+                road_status = random.randint(0, 2)  # 随机道路状态
+                node_index_last = 0    #用来记下要连线的前一个点的索引
+                node_index_now = 0  #用来记下当前节点的索引
+                for index in range(len(coordinates_baidu)):
+                    coord_baidu_str = str(coordinates_baidu[index][0]) + "," + str(coordinates_baidu[index][1])
+                    quiry_index = nodes_index_hashmap.get(coord_baidu_str, -1)
+                    if  quiry_index == -1:
+                        #当前节点未出现过，新存储一个
+                        nodes_index += 1
+                        node_index_now = nodes_index
+                        nodes[str(node_index_now)] = coordinates_baidu[index]
+                        nodes_index_hashmap[coord_baidu_str] = nodes_index
+                    else:   #节点已出现过，查之前出现的索引
+                        count_repeat += 1
+                        if( count_repeat % 10 == 0):
+                            print("count_repeat" + str(count_repeat))
+                        node_index_now = quiry_index
+
+                    if (index != 0) :
+                        # fragment是一个线路片段
+                        fragment = []
+                        fragment_head = {}
+                        fragment_head["name"] = str(node_index_last)
+                        fragment_tail = {}
+                        fragment_tail["name"] = str(node_index_now)
+                        fragment.append(fragment_head)
+                        fragment.append(fragment_tail)
+                        roads[road_status].append(fragment)
+                    
+                    node_index_last = node_index_now
+                # nodes[str(nodes_index)] = coordinates_baidu[len(coordinates_baidu) - 1]
     
+    print("nodes_num:" + str(nodes_index))
+    print("count_repeat:" + str(count_repeat))
     road_file.close()
     
     output_file = codecs.open(output_path, mode='w', encoding='utf-8')
@@ -117,6 +137,7 @@ def get_roads_coordinates(geojson_path, output_path):
     output_str = json.dumps(output)
     output_file.write(output_str)
     output_file.close()
+
 
 def get_roads_coordinates_evaluate(geojson_path, output_path):
     '''从道路数据geojson中取出所需大路的坐标数据，生成echarts需要的格式
@@ -139,11 +160,12 @@ def get_roads_coordinates_evaluate(geojson_path, output_path):
     count = 0
     nodes_set = set()
     count_repeat = 0
+    count_node = 0
     for line in road_file:
         count += 1
         #print(count)
-        #if(count > 1000):
-            #break
+        if(count > 1000):
+            break
         if (line.startswith("{ \"type\": \"Feature\", \"properties\": { \"osm_id\"")):  # 以这个作为道路行数据的标志
             if(line.endswith(',\n')):
                 line = line.rstrip(', \n')  # 去除末尾可能有的逗号
@@ -153,7 +175,7 @@ def get_roads_coordinates_evaluate(geojson_path, output_path):
             if (road_data['properties']['type'] in road_types_needed):  # 只取出大路
                 coordinates_gps = road_data['geometry'] ['coordinates']
                 for coord in coordinates_gps:
-                    
+                    count_node += 1
                     coord_str = str(coord[0]) + ',' + str(coord[1]) 
                     if coord_str in nodes_set:
                         count_repeat += 1
@@ -161,13 +183,22 @@ def get_roads_coordinates_evaluate(geojson_path, output_path):
                     else:
                         nodes_set.add(coord_str);
     print(count_repeat)
-
+    print(count_node)
+    
 def main():
 
-    #get_roads_coordinates("BeijingRoads.json", "data.json")
-    get_roads_coordinates_evaluate("BeijingRoads.json", "data.json")
-    # convert_coordinates("116.29966, 39.96912;116.29966, 39.96912" )
+    get_roads_coordinates("BeijingRoads.json", "data.json")
+    #get_roads_coordinates_evaluate("BeijingRoads.json", "data.json")
     
+    '''
+    result = convert_coordinates("116.29966, 39.96912;116.29966, 39.96912" );
+    print(result)
+    result[0]['x'] = round(result[0]['x'], 5);
+    result[0]['y'] = round(result[0]['y'], 5);
+    result[1]['x'] = round(result[1]['x'], 5);
+    result[1]['y'] = round(result[1]['y'], 5);
+    print(result)
+    '''
     
     
 if __name__ == "__main__":
